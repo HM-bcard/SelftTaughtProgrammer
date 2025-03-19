4c403a2e2dbc4de8600c3d06f37fe6af2e029ab8
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2025-03-19 23:45:28 +0100

    Create cs50cash.py

---
+++ cs50cash.py
@@ -0,0 +1,13 @@
+from cs50 import get_float
+
+change = get_float("Change: ")
+change_to_customer = 0
+
+While True:
+    if change < 0:
+        
+    elif change = 0:
+        print (change_to_customer)
+    else:
+        change = get_float("Change: ")
+
